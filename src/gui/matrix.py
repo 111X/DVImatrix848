864d6dc32dc1f92ee8f5b7a4319775b6fbc616bd
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2015-02-07 10:16:33 +0100

    draft user interface

---
+++ src/gui/matrix.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright © 2014, IOhannes m zmölnig, IEM
+
+# This file is part of HDMIports
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with HDMImatrix.  If not, see <http://www.gnu.org/licenses/>.
+from PySide import QtGui, QtCore
+import matrix_ui
+class matrix(QtGui.QMainWindow, matrix_ui.Ui_MainWindow):
+    def __init__(self,
+                 inputs=["IN1", "IN2", "IN3", "foo", "bar"],
+                 outputs=["OUT1", "OUT2", "OUT3","OUT4"],
+                 ):
+        super(matrix, self).__init__()
+        self.inputs=inputs
+        self.outputs=outputs
+        self.setupUi(self)
+
+        for innum, input in enumerate(inputs):
+            inlabel = QtGui.QLabel(self.groupBox)
+            #inlabel.setObjectName(input)
+            self.gridLayout.addWidget(inlabel, 1+innum, 0, 1, 1)
+            inlabel.setText(input)
+
+        self.outgroup=[]
+        self.out4in=[]
+        
+        for outnum, output in enumerate(outputs):
+            outgroup=QtGui.QButtonGroup(self.groupBox)
+            self.outgroup+=[outgroup]
+            outlabel = QtGui.QLabel(self.groupBox)
+            #outlabel.setObjectName(output)
+            self.gridLayout.addWidget(outlabel, 0, 1+outnum, 1, 1)
+            outlabel.setText(output)
+            self.out4in+=[-1]
+
+            for innum, input in enumerate(inputs):
+                butn=QtGui.QRadioButton(self.groupBox)
+                butn.setText("")
+                self.gridLayout.addWidget(butn, 1+innum, 1+outnum, 1, 1)
+                outgroup.addButton(butn)
+                outgroup.setId(butn, innum)
+                butn.setToolTip("%s -> %s" % (input, output))
+                outgroup.buttonClicked.connect(self.clickedConnection)
+                #print("connected %s for %s" % (outgroup, butn))
+        
+
+        
+    def clickedConnection(self, btn):
+        btngrp=btn.group()
+        innum=btngrp.checkedId()
+        outnum=-1
+        for on,og in enumerate(self.outgroup):
+            if og is btngrp:
+                outnum=on
+                break
+        if self.out4in[outnum] != innum:
+            self.routeInput2Output(innum, outnum)
+
+    def routeInput2Output(self, innum, outnum):
+        self.out4in[outnum]=innum
+        print("connecting: %s -> %s" % (innum, outnum))
+
+    def rescanSerial(self):
+        import serial.tools.list_ports
+        ports=serial.tools.list_ports.comports()
+        for p in ports:
+            print("p=%s" % (p[0],))
+        
+    
+if __name__ == '__main__':
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    window = matrix()
+    window.show()
+    # Run the main Qt loop
+    sys.exit(app.exec_())
