7cd6bdc22e2e893c30487cabc55fbb72eb5cd6d1
IOhannes m zmölnig <zmoelnig@umlautQ-w32.umlaeute.mur.at>
2015-02-16 11:23:22 +0100

    script to check current version against github

---
+++ tests/versioncheck.py
@@ -0,0 +1,41 @@
+
+from distutils.version import LooseVersion
+
+def stripVersionString(version_string):
+    if version_string.startswith('v'):
+	version_string=version_string[1:]
+    return version_string
+
+def getLatestVersion(j):
+	v=None
+	for k in j:
+		version_string=stripVersionString(k['tag_name'])
+		vnew=LooseVersion(version_string)
+		if not v:
+			v=vnew
+		else:
+			
+			if vnew > v:
+				v=vnew
+	if v:
+		return str(v)
+	return None
+def getGithubVersion(project):
+    import requests
+    url=("https://api.github.com/repos/%s/releases" % project)
+    r=requests.get(url)
+    return getLatestVersion(r.json())
+
+def getCurrentVersion():
+    import subprocess
+    tagname=subprocess.check_output(['git', 'describe', '--tags', '--abbrev=0'])
+    return stripVersionString(tagname)
+                                    
+
+if __name__ == '__main__':
+    current_version=getCurrentVersion()
+    github_version=getGithubVersion("iem-projects/DVImatrix848")
+    if LooseVersion(current_version) < LooseVersion(github_version):
+        print("new version available: %s" % (github_version))
+
+    True
